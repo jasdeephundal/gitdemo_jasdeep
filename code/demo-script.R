061df78114d26078d413d8e5c3fbce47e46a5a1b
jasdeephundal <jasdeep.sh@berkeley.edu>
2019-10-03 16:58:31 -0700

    Add demo-script

---
+++ code/demo-script.R
@@ -0,0 +1,12 @@
+#Title: NBA Scores 2018
+#Descritpion: 
+#	This script computes descriptive statisitcs about NBA 2018. 
+#	There are data visualiation examples as well. 
+#Input(s): data file 'nba2018-players.csv'
+#Output(s): summary data files and plots
+#Author: Jasdeep Hundal
+#Date: 10-3-2019
+
+#packages
+library(dplyr)
+library(ggplot2)
