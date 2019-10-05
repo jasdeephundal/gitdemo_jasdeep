ab1aed4bfc51f95e11f0b6a5178e3f21fd03a98d
jasdeephundal <jasdeep.sh@berkeley.edu>
2019-10-04 18:45:33 -0700

    code: Add Everything

--- code/demo-script.R
+++ code/demo-script.R
@@ -10,3 +10,53 @@
 #packages
 library(dplyr)
 library(ggplot2)
+
+read.csv(file="../data/nba2018-players.csv",header=TRUE,sep=",", 
+         col.names=c("player","team","position","height","weight","age","experience","college","salary","games","minutes","points","points3","points2","points1"),
+         colClasses = c(rep("character",3), rep("numeric",4), "character", rep("numeric",7)))
+
+warriors <- dat %>% filter(team=="GSW") %>% arrange(salary)
+warriors
+
+path_out = '../data/'
+fileName = paste(path_out, 'warriors.csv')
+write.csv(warriors,fileName)
+
+#With row.names
+path_out = '../data/'
+fileName = paste(path_out, 'warriors.csv')
+write.csv(warriors,fileName, row.names=FALSE)
+
+#Exporting some R 
+
+sink(file = "../output/data-structure.txt")
+str(dat)
+sink()
+
+sink(file= "../output/summary-warriors.txt")
+summary(warriors)
+sink()
+
+
+LAL <- dat %>% filter(team=="LAL")
+LAL
+
+sink(file="../output/summary-lakers.txt")
+summary(LAL)
+sink()
+
+#8: Exporting Graphs
+
+jpeg(filename = "../images/histogram-age.jpeg", width=400, height=600)
+hist(dat$age, xlab="Age")
+dev.off()
+
+png(filename = "../images/scatterplot2-height-weight.png")
+plot(dat$height, dat$weight, pch = 10, 
+     xlab = 'Height', ylab = 'Weight')
+dev.off()
+
+ggplot(dat, aes(dat$height, dat$weight)) +geom_point(size=2, shape=23 ) +facet_grid(dat$position ~.)
+
+ggsave("../images/height_weight_by_position.pdf", plot = last_plot())
+
